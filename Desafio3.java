b989fd3005feadac585d3a67b9177bb368d5dde4
anamenici <anamenici@gmail.com>
2022-02-16 16:56:20 -0300

    Upload de primeiros passos do desafio 3

---
+++ Desafio3.java
@@ -0,0 +1,27 @@
+package PortoSeguro;
+
+import java.util.Scanner;
+
+public class Desafio3 {
+
+	public static void main(String[] args) {
+		
+		String palavra;
+		int qdeLetras;
+		
+		Scanner ler = new Scanner(System.in) {
+			System.out.println("\nEscreva uma palavra: ");
+			palavra = ler.nextLine();
+		
+		
+		for(int i=0; i<palavra.length(); i++) {
+			
+		}
+		
+		
+		
+		System.out.println("\nO número de combinações de  ");
+		
+	}
+
+}
